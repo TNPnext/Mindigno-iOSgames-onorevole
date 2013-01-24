8b63736130307aa7ce70b1eb0421e869439bb92d
kennymuse <kennymuse@gmail.com>
2013-01-24 15:18:28 +0100

    Initial Commit

---
+++ Barbareschi/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Barbareschi
+//
+//  Created by Enrico on 24/01/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Barbareschi/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Barbareschi
+//
+//  Created by Enrico on 24/01/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
